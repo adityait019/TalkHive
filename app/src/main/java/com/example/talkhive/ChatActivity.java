4323b51c1e4d73d21f464808418b459a088a9b63
adityait019 <aditya.kumar.it24@heritageit.edu.in>
2023-07-05 23:40:48 +0530

    updated some chat related layouts

---
+++ app/src/main/java/com/example/talkhive/ChatActivity.java
@@ -0,0 +1,43 @@
+package com.example.talkhive;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.example.talkhive.adapters.ViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private static final String FRAGMENT_CHAT = "Chat";
+    private static final String FRAGMENT_USERS = "Users";
+    private ViewPager2 viewPager;
+    private TabLayout layout;
+    private ViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        init();
+        viewPager.setAdapter(adapter);
+        new TabLayoutMediator(layout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText(FRAGMENT_CHAT);
+                    break;
+                case 1:
+                    tab.setText(FRAGMENT_USERS);
+                    break;
+            }
+        }).attach();
+    }
+
+    private void init() {
+        viewPager = (ViewPager2) findViewById(R.id.viewPager);
+        layout = (TabLayout) findViewById(R.id.tabLayout);
+        adapter = new ViewPagerAdapter(this);
+    }
+}
